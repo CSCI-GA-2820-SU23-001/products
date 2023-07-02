bf2f20034617bd990017ff9c700b144833359249
Alan-Ou <156554822@qq.com>
2023-07-02 07:57:56 +0000

    initial working version of my new feature

--- service/routes.py
+++ service/routes.py
@@ -29,3 +29,87 @@ def index():
 ######################################################################
 
 # Place your REST API code here ...
+
+######################################################################
+# CREATE A NEW PRODUCT
+######################################################################
+@app.route("/products", methods=["POST"])
+def create_products():
+    """
+    Creates a Product
+    This endpoint will create a Product based the data in the body that is posted
+    """
+    app.logger.info("Request to create a Product")
+    check_content_type("application/json")
+    product = Product()
+    product.deserialize(request.get_json())
+    product.create()
+    message = product.serialize()
+    # TO-DO: When get_product is implemented, uncommented below
+    # location_url = url_for("get_products", product_id=product.id, _external=True)
+    location_url = "not implemented"
+    app.logger.info("Product with ID [%s] created.", product.id)
+    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+
+######################################################################
+# UPDATE A PRODUCT
+######################################################################
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_products(product_id):
+    """
+    Update a Product
+    This endpoint will update a Product based on the body that is posted
+    """
+    app.logger.info("Request to update a product with id: %s", product_id)
+    check_content_type("application/json")
+
+    product = Product.find(product_id)
+    if not product:
+        app.logger.error("Target product is not found")
+        abort(
+            status.HTTP_404_NOT_FOUND, 
+            f"Product with id '{product_id}' is not found.",
+        )
+
+    product.deserialize(request.get_json())
+    product.id = product_id
+    product.update()
+    message = product.serialize()
+
+    app.logger.info("Product with id [%s] is successfully updated.", product.id)
+    return jsonify(message), status.HTTP_200_OK
+
+######################################################################
+# DELETE A PRODUCT
+######################################################################
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """
+    Delete an existing Product
+    This endpoint will delete a Product based on it's id
+    """
+    app.logger.info("Request to delete product with id: %s", product_id)
+    product = Product.find(product_id)
+
+    if product:
+        product.delete()
+
+    return jsonify(message="success"), status.HTTP_204_NO_CONTENT
+
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {content_type}",
+    )
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,7 +12,9 @@ from unittest.mock import MagicMock, patch
 from service import app
 from service.models import db
 from service.common import status  # HTTP Status Codes
+from tests.factories import ProductFactory
 
+BASE_URL = '/products'
 
 ######################################################################
 #  T E S T   C A S E S
@@ -34,6 +36,22 @@ class TestYourResourceServer(TestCase):
 
     def tearDown(self):
         """ This runs after each test """
+    
+    def _create_products(self, count):
+        """ Factory method for batch creation of products """
+        products = []
+        for _ in range(count):
+            test_product = ProductFactory()
+            response = self.client.post(BASE_URL, json=test_product.serialize())
+            self.assertEqual(
+                response.status_code,
+                status.HTTP_201_CREATED,
+                "Couldn't create a test product",
+            )
+            new_product = response.get_json()
+            test_product.id = new_product["id"]
+            products.append(test_product)
+        return products
 
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
@@ -43,3 +61,32 @@ class TestYourResourceServer(TestCase):
         """ It should call the home page """
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_update_product(self):
+        """ It should update an existing product """
+
+        # 1. Create a product to update
+        test_product = ProductFactory()
+        response = self.client.post(BASE_URL, json=test_product.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # 2. Update that product
+        new_product = response.get_json()
+        logging.debug(new_product)
+        new_product["category"] = "category6"
+        response = self.client.put(f"{BASE_URL}/{new_product['id']}", json=new_product)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_product = response.get_json()
+        self.assertEqual(updated_product["category"], "category6")
+
+    def test_delete_products(self):
+        """ It should Delete a Product """
+        test_product = self._create_products(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        
+        # make sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
